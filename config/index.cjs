bffc8cf6ad529261622a30bc9054c5a303af6904
sealien <32618841+sealien@users.noreply.github.com>
2023-05-27 20:36:42 +0800

    Update index.cjs
    add a user

--- config/index.cjs
+++ config/index.cjs
@@ -21,6 +21,38 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: 'oCAJl6NzN5twQsjju-Fk8cQ-RYjc',
+      // 使用微信测试号：你想对他发送的模板消息的模板ID
+      useTemplateId: '9-dQUa-PX0JN96fKGKUQReD73W-FsB-I17waxnyJKy8',
+      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
+      horoscopeDate: '02-10',
+      festivals: [
+        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+        {
+          type: '*生日', name: '宝贝', year: '1994', date: '01-01',
+        },
+        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
+        //{
+        //  type: '生日', name: '李四', year: '1996', date: '09-31',
+        //},
+        {
+          type: '节日', name: '相识纪念日', year: '2019', date: '02-06',
+        },
+      ],
+      // 我们在一起已经有xxxx天了的配置
+      customizedDateList: [
+        // 在一起的日子
+        { keyword: 'love_day', date: '2022-09-08' },
+        // 结婚纪念日
+        { keyword: 'marry_day', date: '2020-10-29' },
+        //出生日期
+        { keyword: 'baby_day', date: '2022-12-07' }
+      ],
+    },
+    {
+      // 想要发送的人的名字
+      name: '自己',
+      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oCAJl6MBcp61Yr0TrEHVFdlx5gwM',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '9-dQUa-PX0JN96fKGKUQReD73W-FsB-I17waxnyJKy8',
